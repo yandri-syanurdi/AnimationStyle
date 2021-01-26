792e18645248bf9a7183a8f869e6672ba33ba85f
yandri-syanurdi <yandri.syanurdi12@gmail.com>
2021-01-26 08:34:44 +0700

    Animation Style

---
+++ gallery-style/gallerysearchfilter/js/main.js
@@ -0,0 +1,19 @@
+// ========================================================================= //
+//  Porfolio isotope and filter
+// ========================================================================= //
+$(window).load(function(){
+
+  var portfolioIsotope = $('.portfolio-container').isotope({
+    itemSelector: '.portfolio-thumbnail',
+    layoutMode: 'fitRows'
+  });
+
+  $('#portfolio-flters li').on( 'click', function() {
+    $("#portfolio-flters li").removeClass('filter-active');
+    $(this).addClass('filter-active');
+
+    portfolioIsotope.isotope({ filter: $(this).data('filter') });
+  });
+
+})
+
